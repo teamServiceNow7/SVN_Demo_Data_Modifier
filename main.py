60bac1374ab4bfb112697fd89072f584143bb5e1
teamServiceNow7 <teamservicenow7@gmail.com>
2024-08-23 15:29:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -625,7 +625,7 @@ def main():
                     deny.update_denial_source()
                 if new_date is not None:
                     error = deny.update_denial_date()
-                tree = deny.generate_xml_from_database()
+                tree = deny.generate_xml_from_database("samp_eng_app_denial")
                 modified_xml = save_modified_xml(file_name, tree)
                 st.sidebar.download_button(
                     label="Download Modified XML",
