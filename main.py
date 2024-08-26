330891667c0d7f91f816808363cdad12b57449d9
teamServiceNow7 <teamservicenow7@gmail.com>
2024-08-26 10:42:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -307,6 +307,7 @@ def main():
 
     upload_button = st.sidebar.button("Upload XML Files", use_container_width=True, type="primary")
     default_button = st.sidebar.button("Use Default Files", use_container_width=True)
+    st.sidebar.divider()
 
     if upload_button:
         with st.sidebar.expander(f"#### UPLOAD FILES", expanded=True):
